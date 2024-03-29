6999d73bc7a57f3d5c1b065b1a5f2cc7be7b2b00
Sameeranjoshi <joshisameeran17@gmail.com>
2024-03-29 14:41:55 -0600

    start working on the external part and not the integrated one, add utilities, a plan for how to do

--- examples/external/main.cpp
+++ examples/external/main.cpp
@@ -6,6 +6,7 @@
 #include "bricksetup.h"
 #include "multiarray.h"
 #include "brickcompare.h"
+#include <string.h>
 
 // Setting for X86 with at least AVX2 support
 #include <immintrin.h>
@@ -41,6 +42,87 @@ for (long k = tk; k < tk + TILE; ++k) \
 for (long j = tj; j < tj + TILE; ++j) \
 _Pragma("omp simd") \
 for (long i = ti; i < ti + TILE; ++i)
+
+// Utilities 
+
+struct global_args {
+  int write_coeff_into_file;
+};
+// global declaration allocates space automatically.
+global_args arg_handler;
+
+void write_coeff_into_file(bElem *coeff){
+    
+    // Open a file for writing
+    std::string filename="coefficients.txt";
+    std::ofstream outfile(filename);
+
+    // Check if file opened successfully
+    if (!outfile.is_open()) {
+        std::cerr << "Unable to open file" << filename << "for writing." << std::endl;
+        exit(1);
+    }
+
+    // Write coefficients to the file
+    for (int i = 0; i < 129; ++i)
+        outfile << coeff[i] << std::endl;
+    
+    std::cout << "\n Written coefficients.txt \n";
+    // Close the file
+    outfile.close();
+}
+
+void handle_coefficient_data(bElem *coeff, global_args *handler){
+
+  
+  std::random_device r;
+  std::mt19937_64 mt(r());
+  std::uniform_real_distribution<bElem> u(0, 1);
+
+  // Randomly initialize coefficient, Towan's way of initialization.
+  for (int i = 0; i < 129; ++i)
+    coeff[i] = u(mt);
+
+  if (handler->write_coeff_into_file){
+    write_coeff_into_file(coeff);
+  }
+
+}
+
+int handle_argument_parsing(int argc, char** argv, global_args *handler) {
+    auto generic_error_msg = [&]() {
+        std::cerr << "Usage: " << argv[0] << " <flags>" << std::endl;
+        std::cerr << "flags = --dump-coeff=<true|false>, ..." << std::endl;
+    };
+
+    // Check the number of command-line arguments
+    if (argc != 2) {
+        generic_error_msg();
+        return 1;
+    }
+
+    // Parse the command-line argument
+    // --dump-coeff=<true|false>
+    std::string arg1 = argv[1];
+    if (arg1.find("--dump-coeff=") == 0) {
+        std::string value = arg1.substr(13); // Skip "--dump-coeff="
+        if (value == "true") {
+            handler->write_coeff_into_file = 1;
+        } else if (value == "false") {
+            handler->write_coeff_into_file = 0;
+        } else {
+            generic_error_msg();
+            return 1;
+        }
+    } else {
+        generic_error_msg();
+        return 1;
+    }
+
+    return 0;
+}
+
+// -- end utilities
 
 template<typename T>
 double time_func(T func) {
@@ -141,17 +223,25 @@ void d3pt7() {
   free(bInfo.adj);
 }
 
-
-int main() {
+int main(int argc, char **argv) {
+  // arg_handler is a global struct containing various flags.
+  global_args arg_handler = {0};
+  handle_argument_parsing(argc, argv, &arg_handler);
+  // allocate space for coefficients
   coeff = (bElem *) malloc(129 * sizeof(bElem));
-  std::random_device r;
-  std::mt19937_64 mt(r());
-  std::uniform_real_distribution<bElem> u(0, 1);
+  handle_coefficient_data(coeff, &arg_handler);
 
-  for (int i = 0; i < 129; ++i)
-    coeff[i] = u(mt);
+  // brick1 = d3pt7();  // dumps and runs first
+  // beick2 = d3pt7();  // reads CDC data and runs second time 
+  // // compare here.
+  // results  = comparenumerically(brick1, brick2);
+  // if (results == true){
+  //     Verified
+  // }
+  // else {
+  //   not correct
+  // }
 
-  d3pt7();
   return 0;
 }
 
