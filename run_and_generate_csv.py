c89dd8a6f656429dbfe1e6d6ed1527515c3697b6
Sameeranjoshi <joshisameeran17@gmail.com>
2024-04-10 12:48:52 -0600

    reference changes

--- run_and_generate_csv.py
+++ run_and_generate_csv.py
@@ -10,4 +10,4 @@ with open("output.txt", "w") as output_file:
         subprocess.run(["/uufs/chpc.utah.edu/common/home/u1418973/other/verify_gravel/bricklib/examples/external/build/example"], stdout=output_file, text=True)
 
 # Run the script to generate the CSV file
-subprocess.run(["python3", "csv_generator.py"])
+subprocess.run(["python3", "csv_excel_generator.py"])
